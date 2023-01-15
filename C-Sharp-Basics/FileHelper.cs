39ee6a05a078fd6daf9e56a09095ff503c54425f
sayan saha <sayan199783@gmail.com>
2023-01-15 16:06:42 +0530

    FileHelper class added for file handling methods

---
+++ C-Sharp-Basics/FileHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace C_Sharp_Basics
+{
+    class FileHelper
+    {
+        public static int GetWordCount(string path)
+        {
+           //string text = File.ReadAllText(path);
+           //string[] splitedText  = text.Split(new char[] { ' ','.'},StringSplitOptions.RemoveEmptyEntries);
+           //return splitedText.Count();
+
+            //Same Code Using FileInfo class
+            FileInfo fi = new FileInfo(path);
+            StreamReader sr = fi.OpenText();
+            string s = "";
+            StringBuilder sb = new StringBuilder("");
+            while ((s = sr.ReadLine()) != null)
+            {
+                sb.Append(s);
+            }
+            string text = sb.ToString();
+            string[] splitedText  = text.Split(new char[] { ' ','.'},StringSplitOptions.RemoveEmptyEntries);
+            return splitedText.Count();
+
+        }
+        public static string WordWithMaxLength(string path)
+        {
+            string text = File.ReadAllText(path);
+            string[] splitedText = text.Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            string strMaxLength = "";
+            int max = 0;
+            foreach(string s in splitedText)
+            {
+                if (s.Length > max)
+                {
+                    max = s.Length;
+                    strMaxLength = s;
+                }
+
+            }
+            return strMaxLength;
+
+        }
+    }
+}
---
+++ C-Sharp-Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Sayan\Desktop\Sample.txt";
+            int count = FileHelper.GetWordCount(path);
+            Console.WriteLine("No of words in the file : {0}", count);
+            string word = FileHelper.WordWithMaxLength(path);
+            Console.WriteLine("Word with maximum length in the file : {0}", word);
+        }
+    }
+}
