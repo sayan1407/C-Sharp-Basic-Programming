e958bce5fe9cb383291a95b92ac76074d866eb72
sayan saha <sayan199783@gmail.com>
2023-05-20 12:58:45 +0530

    Hackerrank problem solve

---
+++ C-Sharp-Basics/HackerRankSolution.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics
+{
+    public class HackerRankSolution
+    {
+        public static void SimpleTextEditor()
+        {
+            int numOfOps = Convert.ToInt32(Console.ReadLine());
+            string str = string.Empty;
+            Stack<string> stack = new Stack<string>();
+            LinkedList<int> linkedList = new LinkedList<int>();
+            for (int i =0; i < numOfOps;i++)
+            {
+                string temp = Console.ReadLine();
+                int operationType;
+                string inputParameter = string.Empty;
+                string[] inputs = temp.Split(' ');
+                operationType = Convert.ToInt32(inputs[0]);
+                if(operationType != 4)
+                    inputParameter = inputs[1];
+
+              
+                switch (operationType)
+                {
+                    case 1:
+                        stack.Push(str);
+                        str = string.Concat(str, inputParameter);
+                        break;
+                    case 2:
+                        stack.Push(str);
+                        str = str.Remove(str.Length - Convert.ToInt32(inputParameter));
+                        break;
+                    case 3:
+                        Console.WriteLine(str[Convert.ToInt32(inputParameter) - 1]);
+                        break;
+                    case 4:
+                        str = stack.Pop();
+                        break;
+
+
+                }
+                
+
+            }
+        }
+        //Watson likes to challenge Sherlock's math ability. 
+        //He will provide a starting and ending value that describe a range of integers, inclusive of the endpoints. 
+        //Sherlock must determine the number of square integers within that range.
+        public static int squares(int a,int b)
+        {
+            int count = 0;
+            //Optimal Solution
+            int lowerRange = (int)Math.Pow(a, 0.5);
+            int upperRange = (int)Math.Pow(b, 0.5);
+            count = upperRange - lowerRange;
+            if (lowerRange * lowerRange == a)
+                count++;
+            //Brute-Force Solution
+            //for(int i = a;i <= b;i++)
+            //{
+            //    double squareRoot = Math.Pow(i, 0.5);
+            //    int integerValueOfSquareRoot = (int)squareRoot;
+            //    if (squareRoot == (double)integerValueOfSquareRoot)
+            //        count++;
+            //}
+            return count;
+        }
+        public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2)
+        {
+            DateTime returnDate = new DateTime(y1, m1, d1);
+            DateTime dueDate = new DateTime(y2, m2, d2);
+            int totalDueDays = (int) (returnDate - dueDate).TotalDays;
+            if (totalDueDays <= 0)
+                return 0;
+            else if (totalDueDays < 30)
+                return totalDueDays * 15;
+            else if (totalDueDays > 30 && totalDueDays < 365)
+                return ((int)totalDueDays / 30) * 500;
+            else
+                return 10000;
+
+        }
+        public static int libraryFineManual(int d1, int m1, int y1, int d2, int m2, int y2)
+        {
+            if(y1 > y2)
+            {
+                return 10000;
+
+            }
+            else
+            {
+                if(m2 == m1)
+                {
+                    if (d1 <= d2)
+                        return 0;
+                    else
+                        return (d1 - d2) * 15;
+                }
+                else
+                {
+                        return (m1 - m2) * 500;
+                }
+
+            }
+            
+        }
+
+    }
+}
--- C-Sharp-Basics/LINQPractice.cs
+++ C-Sharp-Basics/LINQPractice.cs
@@ -68,6 +68,79 @@ namespace C_Sharp_Basics
             }
 
         }
+        public void Joins()
+        {
+            List<Student> students = new List<Student>();
+            List<Stream> streams = new List<Stream>()
+            {
+                new Stream(){ ID = 1, Name = "CSE"},
+                new Stream(){ ID = 2, Name = "IT"},
+                new Stream(){ ID = 3, Name = "EE"}
+            };
+            students.Add(new Student()
+            {
+                ID = 1,
+                City = "Kolkata",
+                Stream_Id = 1,
+                Marks = 100
+            });
+            students.Add(new Student()
+            {
+                ID = 2,
+                City = "Kolkata",
+                Stream_Id = 2,
+                Marks = 65
+            });
+            students.Add(new Student()
+            {
+                ID = 3,
+                City = "Mumbai",
+                Stream_Id = 1,
+                Marks = 80
+            });
+            students.Add(new Student()
+            {
+                ID = 4,
+                City = "Dehli",
+                Stream_Id = 3,
+                Marks = 92
+            });
+            students.Add(new Student()
+            {
+                ID = 5,
+                City = "Mumbai",
+                Stream_Id = 2,
+                Marks = 71
+            });
+            //var studentGroupByCity = students.GroupBy(s => s.City);
+            //foreach(var group in studentGroupByCity)
+            //{
+            //    Console.Write("City : "+group.Key+" ");
+            //    Console.Write("Total Students : "+group.Count()+ " ");
+            //    Console.Write("Student IDs : ");
+            //    foreach(var student in group)
+            //        Console.Write(student.ID+" ");
+            //    Console.WriteLine();
+            //}
+            var studentGroupByStream = streams.GroupJoin(students, a => a.ID, b => b.Stream_Id, (stream, student) => new
+            {
+                StreamName = stream.Name,
+                Students_Count = student.Count()
+
+            });
+            //foreach(var group in studentGroupByStream)
+            //{
+            //    Console.Write(group.StreamName+ " ");
+            //    foreach(var s in group.Students)
+            //    {
+            //        Console.Write(s.ID + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            LINQPractice.NthHighestMarks(students, 4);
+            Console.WriteLine(LINQPractice.ConvetListToString(new List<string>() { "Sayan", "Riya", "Prabir" }));
+            LINQPractice.CountFileByExtension(@"C:\Users\Sayan\Desktop");
+        }
     }
 
 }
